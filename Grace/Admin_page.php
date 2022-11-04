d4799ea15fecd6f58e642bb7db2ba2a568b80f2d
alyssacazarte <leenunuyaa@gmail.com>
2022-11-04 11:05:36 +0800

    final commit

---
+++ Grace/Admin_page.php
@@ -0,0 +1,102 @@
+<?php
+$msg = "test";
+$object = new stdClass();
+if (isset($_POST['submit'])) {
+    $forms = array(
+        'firstname'  => $first_name = $_POST['first_name'],
+        'lastname' => $last_name = $_POST['last_name'],
+        'address' => $address = $_POST['address'],
+        'email' => $email = $_POST['email'],
+        'password' => $password = $_POST['password'],
+        'cpassword' => $cpassword = $_POST['cpassword']
+    );
+    foreach ($forms as $key => $value) {
+        if (empty($value)) {
+            $object->$key =  $key . ' field is required';
+        }
+
+        if (!empty($value) && ($key === 'firstname' || $key  === 'lastname')) {
+            if (strlen($value) < 2) $object->$key =  $key . ' must be 2 or more letters';
+        }
+
+        if ($key === 'email' && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $object->$key = "Invalid email format";
+        }
+        if ($key === 'password'){
+            $forms['password'] === $forms['cpassword'] ? '' : $object->$key = "Password didn't match!";
+        }
+    }
+
+    if (!( (array) $object)) header("Location: Dashboard.php?" . http_build_query($forms));
+
+    echo "<script> console.log(" . json_encode($object) . ")</script>";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Handler.css">
+    <title>Registration Form</title>
+</head>
+
+<body background="background1.jpg">
+
+    <div class="wrapper">
+        <h2>Registration Form</h2>
+        <form action="" method="post" class="form">
+            <div class="input-field">
+                <label for="first_name" class="input-label">First Name</label>
+                <input type="text" name="first_name" class="input" id="firstname" value="<?php if (isset($_POST['submit'])) {  echo $first_name;} ?>" />
+                <div style="text-transform: capitalize; color: #FF7F7F; padding-top: 5px; padding-bottom: 5px"><?php echo isset($object->firstname) ? $object->firstname : '' ?></div>
+            </div>
+            <div class="input-field">
+                <label for="last_name" class="input-label">Last Name</label>
+                <input type="text" name="last_name" class="input" id="lastname" value="<?php if (isset($_POST['submit'])) { echo $last_name; } ?>">
+                <div style="text-transform: capitalize; color: #FF7F7F; padding-top: 5px; padding-bottom: 5px">
+                    <?php echo isset($object->lastname) ? $object->lastname : '' ?>
+                </div>
+
+            </div>
+            <div class="input-field">
+                <label for="address" class="input-label">Address</label>
+                <input type="text" name="address" class="input" id="address" value="<?php if (isset($_POST['submit'])) { echo $address; } ?>">
+                <div style="text-transform: capitalize; color: #FF7F7F; padding-top: 5px; padding-bottom: 5px">
+                    <?php echo isset($object->address) ? $object->address : '' ?>
+                </div>
+
+            </div>
+            <div class="input-field">
+                <label for="email" class="input-label">Email</label>
+                <input type="email" name="email" class="input" id="email" value="<?php if (isset($_POST['submit'])) { echo $email; } ?>">
+                <div style="text-transform: capitalize; color: #FF7F7F; padding-top: 5px; padding-bottom: 5px"><?php echo isset($object->email) ? $object->email : '' ?>
+                </div>
+
+            </div>
+            <div class="input-field">
+                <label for="Password" class="input-label">Password</label>
+                <input type="password" name="password" class="input" id="password" value="<?php if (isset($_POST['submit'])) { echo $password; } ?>">
+                <div style="text-transform: capitalize; color: #FF7F7F; padding-top: 5px; padding-bottom: 5px">
+                    <?php echo isset($object->password) ? $object->password : '' ?>
+                </div>
+
+            </div>
+            <div class="input-field">
+                <label for="cpassword" class="input-label">Confirm Password</label>
+                <input type="password" name="cpassword" class="input" id="cpassword" value="<?php if (isset($_POST['submit'])) {
+                                                                                                echo $cpassword;
+                                                                                            } ?>">
+                <div style="text-transform: capitalize; color: #FF7F7F; padding-top: 5px; padding-bottom: 5px">
+                    <?php echo isset($object->cpassword) ? $object->cpassword : '' ?>
+                </div>
+            </div>
+            <button type="submit" name="submit" class="btn">Register</button>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ Grace/Dashboard.php
@@ -0,0 +1,52 @@
+<?php
+// echo $_GET['link'];
+
+$firstname = $_GET['firstname'];
+$lastname = $_GET['lastname'];
+$address = $_GET['address'];
+$email = $_GET['email'];
+$password = $_GET['password'];
+$cpassword = $_GET['cpassword'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Handler.css">
+    <title>Dashboard</title>
+</head>
+
+<body background = 'bts.jpg'>
+    <div class="wrapper">
+        <h2>You are now Registered</h2>
+        <div>
+            <div style="margin: 10px;">
+                <label for="first_name" class="input-label">First Name:</label>
+                <p style="float: right; font-weight: 100"><?php echo (isset($firstname))?$firstname:'';?></p>
+                <div style="text-transform: capitalize; color: #FF7F7F; padding-top: 5px; padding-bottom: 5px"><?php echo isset($object->firstname) ? $object->firstname : '' ?></div>
+            </div>
+            <div style="margin: 10px;">
+                <label for="last_name" class="input-label">Last Name:</label>
+                <p style="float: right; font-weight: 100"><?php echo (isset($lastname))?$lastname:'';?></p>
+                <div style="text-transform: capitalize; color: #FF7F7F; padding-top: 5px; padding-bottom: 5px"><?php echo isset($object->lastname) ? $object->lastname : '' ?></div>
+            </div>
+            <div style="margin: 10px;">
+                <label for="address" class="input-label">Address:</label>
+                <p style="float: right; font-weight: 100"><?php echo (isset($address))?$address:'';?></p>
+                <div style="text-transform: capitalize; color: #FF7F7F; padding-top: 5px; padding-bottom: 5px"><?php echo isset($object->address) ? $object->address : '' ?></div>
+            </div>
+            <div style="margin: 10px;">
+                <label for="email" class="input-label">Email:</label>
+                <p style="float: right; font-weight: 100"><?php echo (isset($email))?$email:'';?></p>
+                <div style="text-transform: capitalize; color: #FF7F7F; padding-top: 5px; padding-bottom: 5px"><?php echo isset($object->email) ? $object->email : '' ?></div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
